ebcc05d87386dad9a85c909c43c2de6ee576342f
twichai <twichai4869@gmail.com>
2025-04-03 22:29:48 +0700

    Add nav mockup

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from '../../components/nav/nav.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterOutlet, NavComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
