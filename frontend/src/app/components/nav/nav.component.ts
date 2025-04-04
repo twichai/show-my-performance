b241660fc25f7267ea205a94d5ee9fa7dfab318b
twichai <twichai4869@gmail.com>
2025-04-04 23:19:38 +0700

    update nav

--- frontend/src/app/components/nav/nav.component.ts
+++ frontend/src/app/components/nav/nav.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.css'
+  styleUrl: './nav.component.css',
 })
 export class NavComponent {
+  router = inject(Router);
+  goToCreatePost() {
+    this.router.navigate(['/create-post']);
+  }
 
+  logout() {
+    localStorage.removeItem('token'); // Remove user token
+    this.router.navigate(['/login']); // Redirect to login page
+  }
 }
