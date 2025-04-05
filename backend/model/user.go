13f9d5012d3cee1ef9468427da4ef9ebc06ef338
twichai <twichai4869@gmail.com>
2025-04-05 17:34:03 +0700

    update User json

--- backend/model/user.go
+++ backend/model/user.go
@@ -8,11 +8,11 @@ type User struct {
 	gorm.Model
 	Username        string `json:"username"`
 	Password        string `json:"password"`
-	ProfileImageURL string `json:"profile_image_url"`
+	ProfileImageURL string `json:"pofileImageUrl"`
 	Email           string `json:"email" gorm:"uniqueIndex"`
-	FirstName       string `json:"first_name"`
-	LastName        string `json:"last_name"`
-	PhoneNumber     string `json:"phone_number"`
+	FirstName       string `json:"firstName"`
+	LastName        string `json:"lastName"`
+	PhoneNumber     string `json:"phoneNumber"`
 	Posts           []Post `json:"posts" gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE"`
 }
 
