136bca9c73f96df03c6d6b7f310c84a31fae0ca9
twichai <twichai4869@gmail.com>
2025-04-25 05:32:45 +0700

    fix update post where have image

--- backend/core/post/post_service.go
+++ backend/core/post/post_service.go
@@ -11,7 +11,7 @@ type PostService interface {
 	GetAllPosts() ([]model.Post, error)
 	GetPostByID(id uint) (*model.Post, error)
 	CreatePost(post *model.Post, file []*multipart.FileHeader) (*model.Post, error)
-	UpdatePost(post *model.Post) (*model.Post, error)
+	UpdatePost(post *model.Post, file []*multipart.FileHeader) (*model.Post, error)
 	DeletePost(id uint) error
 	GetPostsByUserID(userID uint) ([]model.Post, error)
 }
@@ -78,7 +78,7 @@ func (p *postServiceImpl) GetPostsByUserID(userID uint) ([]model.Post, error) {
 }
 
 // UpdatePost implements PostService.
-func (p *postServiceImpl) UpdatePost(post *model.Post) (*model.Post, error) {
+func (p *postServiceImpl) UpdatePost(post *model.Post, file []*multipart.FileHeader) (*model.Post, error) {
 	fmt.Println(post.ID)
 	oldPost, err := p.repo.GetPostByID(post.ID)
 	if err != nil {
@@ -86,6 +86,17 @@ func (p *postServiceImpl) UpdatePost(post *model.Post) (*model.Post, error) {
 	}
 	if oldPost.UserID != model.CurrentUser.ID {
 		return nil, fmt.Errorf("you are not authorized to update this post")
+	}
+
+	if len(file) > 0 {
+		s3Repository := s3Adapter.NewS3Repository("https://show-my-performance.s3.amazonaws.com/", "us-east-1")
+		url, err := s3Repository.UploadFile(file, post.Title)
+		if err != nil {
+			return nil, err
+		}
+		if url != "" {
+			oldPost.ImageUrl = url
+		}
 	}
 	oldPost.Title = post.Title
 	oldPost.Content = post.Content
